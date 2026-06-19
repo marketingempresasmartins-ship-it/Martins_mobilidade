741c4d62f77c6cbfdd5e4ed2cfdd47d717949f7d
Lucassbento25 <lucassbento25@gmail.com>
2026-06-19 14:59:21 -0400

    fix: ajustar estilos do aviso de preview e corrigir link do instagram

--- src/config/martinsConfig.js
+++ src/config/martinsConfig.js
@@ -2,7 +2,7 @@ const env = import.meta.env || {};
 
 export const MARTINS_CONFIG = {
   whatsappNumber: env.VITE_WHATSAPP_NUMBER || "5592992925353",
-  instagramUrl: env.VITE_INSTAGRAM_URL || "https://instagram.com/martinsmobilidade",
+  instagramUrl: env.VITE_INSTAGRAM_URL || "https://www.instagram.com/martins.mob",
   email: env.VITE_CONTACT_EMAIL || "",
   address: env.VITE_ADDRESS || "Av. Torquato Tapajós, 5552 - Flores, Manaus - AM, 69058-830",
   leadEndpoint: env.VITE_LEAD_ENDPOINT || ""
